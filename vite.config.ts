18fcff72a411d6fd3dd9acd78202969eb75d12f7
Bun <bun2932005@gmail.com>
2024-12-28 20:16:27 +0700

    config environment

---
+++ vite.config.ts
@@ -0,0 +1,26 @@
+import { defineConfig } from 'vite';
+
+
+export default defineConfig({
+
+   base: '/',
+   root: 'src',
+   build: {
+      target: "esnext",
+      outDir: '../dist'
+   },
+
+   resolve: {
+      alias: {
+         "@": "",
+      },
+   },
+   server: {
+      host: '0.0.0.0',
+      port: process.env.PORT ? parseInt(process.env.PORT) : 5173,
+   },
+})
+
+
+
+
