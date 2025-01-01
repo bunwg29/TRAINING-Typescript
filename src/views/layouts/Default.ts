208a6ded4b5683043a460ae93e7536275a1f1306
Bun <bun2932005@gmail.com>
2025-01-01 16:44:36 +0700

    style: layout default

---
+++ src/views/layouts/Default.ts
@@ -0,0 +1,48 @@
+import { Header } from '../components';
+import { TableOption } from '../components/Table/TableHeading/TableOption';
+
+// This class is a layout that will be used to render the default layout of the application.
+export class Default {
+  constructor() {}
+
+  /**
+   *
+   * @param component - This is HTML Element that will be wrapped in a div with the class name 'component'.
+   * @returns {HTMLElement} - Returns a div element with the class name 'component'.
+   */
+  private divComponent(component: HTMLElement) {
+    const div = document.createElement('div');
+    div.className = 'component';
+    div.replaceChildren(component);
+
+    return div;
+  }
+
+  /**
+   *
+   * @param childComponent - This is HTML Element that will be wrapped in a div with the class name 'root'.
+   * @returns {HTMLElement} - Returns a div element with the class name 'root'.
+   */
+  private DefaultContent(childComponent?: HTMLElement): HTMLElement {
+    const divDefault = document.createElement('div');
+    divDefault.className = 'root';
+
+    const main = document.createElement('main');
+    main.appendChild(new TableOption().render());
+    main.appendChild(this.divComponent(childComponent as HTMLElement));
+
+    divDefault.appendChild(new Header().render());
+    divDefault.appendChild(main);
+
+    return divDefault as HTMLElement;
+  }
+
+  /**
+   *
+   * @param childComponent - This is HTML Element that will be wrapped in a div with the class name 'root'.
+   * @returns {HTMLElement} - Returns a div element with the class name 'root'.
+   */
+  public render(childComponent?: HTMLElement): HTMLElement {
+    return this.DefaultContent(childComponent as HTMLElement);
+  }
+}
